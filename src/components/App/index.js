bccd61ef117ebd1539af07dfdd378ad7c060cdf9
Elodie Muller <elodiemuller.pro@outlook.com>
2021-05-05 12:02:27 +0200

    creation of message state

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -14,16 +14,18 @@ import './styles.scss';
 import Header from '../Header';
 import Repos from '../Repos';
 //import my data 
-import data from 'src/data/repos'
+//import data from 'src/data/repos'
 
 const BASE_URL = 'https://api.github.com/search/repositories?q='
 // == Composant
 const App = () => {
 
+  const [message, setMessage] = useState('Aucun résultat pour le moment');
+
   const [loading, setLoading] = useState(false)
 
   // state for stock up my list 
-  const [repos, setRepos] = useState(data.items);
+  const [repos, setRepos] = useState([]);
 
   // i want to have in sotkc my input, and i want the "method" to change him.
   const [inputText, setInputText] = useState('');
@@ -47,6 +49,8 @@ const App = () => {
     //console.log(res.data)
     // i put the repos in the state 
     setRepos(res.data.items);
+    const newMessage = `La recherche a donné ${res.data.total_count} repos`;
+    setMessage(newMessage);
   })
   // if not, send a error message 
     .catch((err) => {
@@ -66,7 +70,7 @@ return (
   inputValue={inputText}
   onInputChange={handleInputChange}
   onFormSubmit={handleFormSubmit}/> 
-  <Message content="Je suis un message" />
+  <Message content={message}/>
   {/* My repos need my data, and they need items from the data file */}
   <Repos list={repos} /> 
   </div>
